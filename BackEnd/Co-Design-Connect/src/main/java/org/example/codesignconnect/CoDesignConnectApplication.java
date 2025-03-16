bc12d08fda9b6c0108ce2a850a91deaae8909bab
Pharus <771636307@qq.com>
2025-03-16 22:43:53 +1100

    setup

---
+++ BackEnd/Co-Design-Connect/src/main/java/org/example/codesignconnect/CoDesignConnectApplication.java
@@ -0,0 +1,13 @@
+package org.example.codesignconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoDesignConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoDesignConnectApplication.class, args);
+    }
+
+}
