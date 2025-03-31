e1ab9d3570db6e578b5465d2d6dd576e02dd675d
Pharus <771636307@qq.com>
2025-03-31 20:31:12 +1100

    3/31

--- BackEnd/Co-Design-Connect/src/main/java/org/example/codesignconnect/controller/UserController.java
+++ BackEnd/Co-Design-Connect/src/main/java/org/example/codesignconnect/controller/UserController.java
@@ -20,26 +20,31 @@ public class UserController {
 
     @PostMapping("/login")
     public Result login(String email, String password){
+        log.info("/login: {}, {}", email, password);
         return userService.comparePassword(email, password);
     }
 
     @PostMapping("/register")
     public Result signup(SignupRequest request){
+        log.info("/register: {}", request);
         return userService.signup(request);
     }
 
     @PostMapping("/inviteCode")
     public Result generateCode(String email, Short type){
+        log.info("/inviteCode: {}, {}", email, type);
         return userService.generateCode(email, type);
     }
 
     @PostMapping("/password/resetCode")
     public Result sendVerificationCode(String email){
+        log.info("/password/resetCode: {}", email);
         return emailService.sendVerificationCode(email);
     }
 
     @PostMapping("/password/update")
     public Result resetPassword(String email, String verificationCode, String newPassword){
+        log.info("/password/update: {}, {}, {}", email, verificationCode, newPassword);
         return userService.resetPassword(email, verificationCode, newPassword);
     }
 }
