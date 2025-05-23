dfc4ec6ba83d63f8d7a4315878f8e1b419568ba0
Pharus <771636307@qq.com>
2025-05-23 17:57:03 +1000

    5.23

--- BackEnd/Co-Design-Connect/src/main/java/org/example/codesignconnect/service/impl/PostServiceImpl.java
+++ BackEnd/Co-Design-Connect/src/main/java/org/example/codesignconnect/service/impl/PostServiceImpl.java
@@ -3,8 +3,10 @@ package org.example.codesignconnect.service.impl;
 import org.example.codesignconnect.dto.PostDetail;
 import org.example.codesignconnect.mapper.PostMapper;
 import org.example.codesignconnect.mapper.ReplyMapper;
+import org.example.codesignconnect.mapper.UserMapper;
 import org.example.codesignconnect.model.Post;
 import org.example.codesignconnect.model.Reply;
+import org.example.codesignconnect.model.User;
 import org.example.codesignconnect.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,6 +23,9 @@ public class PostServiceImpl implements PostService {
     @Autowired
     private ReplyMapper replyMapper;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @Override
     public int addPost(Post post) {
         return postMapper.insertPost(post);
@@ -32,7 +37,8 @@ public class PostServiceImpl implements PostService {
         List<PostDetail> postDetails = new ArrayList<>();
         for(Post post : posts){
             List<Reply> replies = replyMapper.getRepliesByPostId(post.getId());
-            postDetails.add(new PostDetail(post, replies));
+            User user = userMapper.getUserById(post.getAuthorId());
+            postDetails.add(new PostDetail(post, replies, user.getUsername()));
         }
         return postDetails;
     }
