8a735369172389c37a1b6973b90baa2dd2abc3eb
zetovicmarko1 <zetovicmarko1@gmail.com>
2025-09-19 16:23:49 +1000

    Replace update request getters/setters with lombok data annotation

--- BackEnd/src/main/java/org/sds/elevateconnect/dto/UserUpdateRequest.java
+++ BackEnd/src/main/java/org/sds/elevateconnect/dto/UserUpdateRequest.java
@@ -1,40 +1,11 @@
 package org.sds.elevateconnect.dto;
 
+import lombok.Data;
+
+@Data
 public class UserUpdateRequest {
     private Integer id;
     private String email;
     private String firstName;
     private String lastName;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 }
