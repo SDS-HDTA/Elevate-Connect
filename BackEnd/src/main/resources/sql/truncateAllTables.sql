78d9a13690ecd4dbbdb09e3ff424b1a61ebb9a27
Matt <matthew@adler.id.au>
2025-11-08 15:45:54 +1100

    SQL: Create script to empty tables

---
+++ BackEnd/src/main/resources/sql/truncateAllTables.sql
@@ -0,0 +1,18 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+TRUNCATE TABLE elevate_connect.community;
+TRUNCATE TABLE elevate_connect.country;
+TRUNCATE TABLE elevate_connect.file;
+TRUNCATE TABLE elevate_connect.invite_codes;
+TRUNCATE TABLE elevate_connect.iteration;
+TRUNCATE TABLE elevate_connect.marker;
+TRUNCATE TABLE elevate_connect.post;
+TRUNCATE TABLE elevate_connect.project;
+TRUNCATE TABLE elevate_connect.project_member;
+TRUNCATE TABLE elevate_connect.reply;
+TRUNCATE TABLE elevate_connect.tasks;
+TRUNCATE TABLE elevate_connect.token;
+TRUNCATE TABLE elevate_connect.user;
+TRUNCATE TABLE elevate_connect.verification_codes;
+
+SET FOREIGN_KEY_CHECKS=1;
